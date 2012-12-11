434a79708855dd83564cf4e93942558d2b681914
William Wolf <throughnothing@gmail.com>
2012-12-11 14:37:31 -0800

    Initial Prototype.

--- lib/Dancer/Plugin/StatsD.pm
+++ lib/Dancer/Plugin/StatsD.pm
@@ -1,7 +1,63 @@
 use strict;
 use warnings;
 package Dancer::Plugin::StatsD;
+use Dancer;
+use Dancer::Plugin;
+use Etsy::StatsD;
 
-# ABSTRACT:
+# ABSTRACT: Dancer Plugin for StatsD support
+
+my $statsd;
+
+# Create statsd object, or return existing one
+sub statsd_obj {
+    # Return it if we got it
+    return $statsd if $statsd;
+
+    my $config = plugin_setting;
+    my $host = $config->{host};
+    my $port = $config->{port};
+    my $sample_rate = $config->{sample_rate} // 1;
+
+    die "No StatsD Host/Port found!" unless $host && $port;
+
+    return $statsd = Etsy::StatsD->new( $host, $port, $sample_rate );
+}
+
+register statsd    => sub { statsd_obj };
+register increment => sub { statsd_obj->increment( @_ ) };
+register decrement => sub { statsd_obj->decrement( @_ ) };
+register update    => sub { statsd_obj->update( @_ ) };
+register timing    => sub { statsd_obj->timing( @_ ) };
+
+register_plugin;
 
 1;
+
+=head1 SYNOPSIS
+
+L<Dancer::Plugin::StatsD> is a L<Dancer> plugin that lets you log events and
+track times using C<StatsD>.
+
+    use Dancer;
+    use Dancer::Plugin::StatsD qw( statsd increment decrement update timing );
+    use Time::HiRes qw( time );
+
+    hook before_error_renden => sub {
+        my ($err) = @_;
+        increment( 'errors.' . $err->code );
+    };
+
+    get '/' => sub {
+        # Increment the homepage hits counter
+        increment( 'hits.homepage' );
+
+        my $t1 = time;
+
+        # Do something that takes a while
+
+        # Log the time taken in ms
+        timing( 'something.slow', (time - $t1) / 1000 );
+    };
+
+    dance;
